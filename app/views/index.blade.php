09b5f933e5f945a9bd6364517ae34e9bfef589dc
David Becerra <dbecerra@college.harvard.edu>
2014-07-28 19:27:52 -0400

    Added form to homepage

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -2,5 +2,12 @@
 
 @section('content')
 
+	{{ Form::open(array('url' => '/pokemon', 'method' => 'POST'))}}
+
+		{{ Form::text('query', null, array('placeholder' => 'Enter Pok&eacute;mon name')) }}
+
+		{{ Form::submit('Search') }}
+
+	{{ Form::close() }}
 
 @stop
