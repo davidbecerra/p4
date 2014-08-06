52fc6fe0b2a3ac7913422de15178981cc484a31b
David Becerra <dbecerra@college.harvard.edu>
2014-08-05 23:37:20 -0400

    More css on pokemon display page

--- app/views/pokemon_display.blade.php
+++ app/views/pokemon_display.blade.php
@@ -9,6 +9,20 @@ Pok&eacute;mon | {{ $pokemon->name }}
 @stop
 
 
+<?php
+	function match_cases($input) {
+		switch ($input) {
+			case -1:
+				return '-';
+			case -2:
+				return '&infin;';
+			default:
+				return $input;
+		}
+	}
+?>
+
+
 @section('content')
 	
 	<div class='pokemon-basic'>
@@ -18,7 +32,7 @@ Pok&eacute;mon | {{ $pokemon->name }}
 			<img src={{ $pokemon->image}}><br>
 		</div>
 
-		<div class='general-table'>
+		<div class='general-table background-color-{{ strtolower($pokemon->types->first()->name) }}'>
 			<table class='general-table'>
 				<thead>
 					<tr><th colspan='2'>General Information</th></tr>
@@ -46,12 +60,28 @@ Pok&eacute;mon | {{ $pokemon->name }}
 
 	</div>
 
-	<br><br>
-	<div class='moves-table'>
-		<table>
-			@foreach ($pokemon->moves as $move)
-				<tr><td>{{ $move->pivot->level }}</td><td>{{ $move->name }}</td></tr>
-			@endforeach
+	<div class='moves-table background-color-{{ strtolower($pokemon->types->first()->name) }}'>
+		<table class='moves-table'>
+			<thead>
+				<tr><th colspan='7'>Moves List (by level up)</th></tr>
+			</thead>
+			<tbody>
+				<tr><th>Level</th><th>Move</th><th>Type</th><th>Category</th><th>Power</th><th>Accuracy (%)</th><th>PP</th></tr>
+				@foreach ($pokemon->moves as $move)
+					<tr>
+						<td>{{ $move->pivot->level }}</td>
+						<td>{{ $move->name }}</td>
+						<td id="background-color-{{ strtolower($move->type->name) }}">{{ $move->type->name }}</td>
+						<td>{{ ucfirst($move->category) }}</td>
+						<td>{{ match_cases($move->power) }}</td>
+						<td>{{ match_cases($move->accuracy) }}</td>
+						<td>{{ $move->PP }}</td>
+					</tr>
+				@endforeach
+			</tbody>
+			<tfoot><tr><td><br></td></tr></tfoot>
 		</table>
 	</div>
+
+	<br><br><br><br><br>
 @stop
